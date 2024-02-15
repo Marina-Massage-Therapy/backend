b0616cb4e846f5bbd43dff349020c17ef9d9e209
OliaK <olgamr@walla.com>
2024-02-15 13:52:22 +0200

    Code cleaning

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from "express";
 import { fetchUser } from "../controllers/user.js";
 
-const router = express.Router(); // Changed Router() to express.Router()
+const router = express.Router();
 router.post("/fetchUser", fetchUser);
 export default router;
