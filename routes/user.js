32b45422b92132ad83aee7e4ec56d242cdfa69ab
OliaK <olgamr@walla.com>
2024-03-19 01:41:12 +0200

    QA

--- routes/user.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { fetchUser } from "../controllers/user.js";
-
-const router = express.Router();
-router.post("/fetchUser", fetchUser);
-export default router;
